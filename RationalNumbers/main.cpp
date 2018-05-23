2447a7d55ce937e7761111dd590712ebb98ebf6f
cpoles <carlos.poles@griffithuni.edu.au>
2018-05-23 21:50:01 +1000

    Initial Commit

---
+++ RationalNumbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RationalNumbers
+//
+//  Created by Carlos Poles on 23/5/18.
+//  Copyright © 2018 Carlos Poles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
